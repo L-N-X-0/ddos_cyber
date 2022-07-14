08eeef4480dd1e8a75d96269cc2daa1b89452ee3
Mahmoud Kamal <67784660+L-N-X-0@users.noreply.github.com>
2022-07-14 20:48:14 +0200

    Add files via upload

---
+++ install.py
@@ -0,0 +1,13 @@
+import os
+
+print("Downloading ...")
+os.system("pip2 install urllib2")
+print("Done Downloaded urllib2")
+os.system("pip2 install sys")
+print("Done Downloaded sys")
+os.system("pip2 install threading")
+print("Done Downloaded threading")
+os.system("pip2 install random")
+print("Done Downloaded random")
+os.system("pip2 install re")
+print("Successed")
---
+++ start.py
@@ -0,0 +1,3 @@
+import os
+url = input("Enter URL Target :")
+os.system("python2 hulk.py "+url)
